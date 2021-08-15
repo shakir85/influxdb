b46d640c49554c137e427d9cc09eb747565f1afa
shakir85 <sralmasaari@gmail.com>
2021-08-15 00:03:37 -0500

    Root folder changes

--- testing/test_api.py
+++ testing/test_api.py
@@ -4,3 +4,20 @@ from src.weather.weather_data import weather_data
 """
 test api
 """
+
+import schedule
+import time
+
+"""
+Script to run at container startup to schedule main.py execution time.
+"""
+
+
+def job(user, password):
+    print(f"I'm working...{user} {password}")
+
+
+schedule.every(3).seconds.do(job, user='foo', password='bar')
+
+while True:
+    schedule.run_pending()
