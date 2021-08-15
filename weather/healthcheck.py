f96805e3977c4a3f7a6c00ba412d4b8578fb2e40
shakir85 <sralmasaari@gmail.com>
2021-08-15 03:47:48 -0500

    Clean up

--- weather/healthcheck.py
+++ weather/healthcheck.py
@@ -4,3 +4,23 @@ Script to periodically test:
     - API connectivity.
     - DB connection.
 """
+
+"""
+#!/bin/bash
+
+datetime=$(date --rfc-3339=s)
+touch /weather/log/startup.log
+
+# Check valid api
+API=$(curl -Is --request GET \
+--url 'https://yahoo-weather5.p.rapidapi.com/weather?lat=37.372&long=-122.038&format=json&u=f' \
+--header 'x-rapidapi-host: yahoo-weather5.p.rapidapi.com' \
+--header 'x-rapidapi-key: ' \
+| head -n 1|cut -d$' ' -f2)
+if [ $API -ne 200 ]; then
+  echo "$datetime - ERROR - Weather API failed, status code: $API" >> /weather/log/startup.log
+  exit 1
+else
+  echo "$datetime - INFO - Weather API valid, status code: $API" >> /weather/log/startup.log
+fi
+"""
--- weather/main.py
+++ weather/main.py
@@ -72,7 +72,7 @@ if __name__ == "__main__":
 
     time.sleep(5)
 
-    schedule.every(2).minutes.do(main, username=args.username, password=args.password, api_key=args.api_key)
-    # schedule.every().day.at("6:00").do(main, username=args.username, password=args.password, api_key=args.api_key)
+    # schedule.every(2).minutes.do(main, username=args.username, password=args.password, api_key=args.api_key)
+    schedule.every().day.at("6:00").do(main, username=args.username, password=args.password, api_key=args.api_key)
     while True:
         schedule.run_pending()
